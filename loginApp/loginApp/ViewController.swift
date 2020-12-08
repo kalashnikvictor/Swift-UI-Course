9590c75329b58f2094488e9d17b5cfdd9be96f72
Victor Kalashnik <kalashnikvictor@gmail.com>
2020-12-08 22:48:43 +0300

    lesson 1
    all good

---
+++ loginApp/loginApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  loginApp
+//
+//  Created by Виктор Калашник on 08.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
+
